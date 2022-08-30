eace6ad83280c9ffa0322b7054f1ee31b66a29a9
Dmytro Tristan <dmytro.tristan@dxc.com>
2022-08-30 17:43:01 +0200

    Separated into different scripts

---
+++ apt_installs.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+printStatus() {
+    #if [[ $VERBOSE == true ]]; then
+    printf "[STATUS] %s\n" "${1}"
+    #fi
+}
+
+printError() {
+    printf "[ERROR] %s\n" "${1}"
+}
+
+printWarning() {
+    printf "[WARNING] %s\n" "${1}"
+}
+
+SUDO=""
+
+if [[ $EUID != 0 ]]; then
+    SUDO="sudo"
+fi
+
+if ! command curl -version &>/dev/null; then
+    printStatus "Curl is not installed but needed for this script"
+    printStatus "Installing Curl"
+    ${SUDO} apt install curl
+fi
+
+INTERNET_STATUS=$(curl http://www.msftncsi.com/ncsi.txt)
+if [[ "${INTERNET_STATUS}" != "Microsoft NCSI" ]]; then
+    printError "Internet is not present\nExiting"
+    exit 0
+fi
+
+if ! command ssh -V &>/dev/null; then
+    printStatus "Installing ssh"
+    sudo apt install openssh-server
+    printStatus "Enabling ssh"
+    sudo systemctl enable ssh
+else
+    printStatus "Openssh-server is already installed"
+fi
+
+printStatus "Installing build-essential kit"
+${SUDO} apt install build-essential
+
+if ! command git version &>/dev/null; then
+    printStatus "Installing git"
+    ${SUDO} apt install git-all
+else
+    printStatus "Git is already installed"
+fi
+
+if ! command ninja --version &>/dev/null; then
+    printStatus "Installing ninja"
+    ${SUDO} apt install ninja-build
+else
+    printStatus "Ninja is already installed"
+fi
+
+if ! command python3 --version &>/dev/null; then
+    printStatus "Installing python3"
+    ${SUDO} apt install python3
+else
+    printStatus "Python3 is already installed"
+fi
+
+if ! command meld --version &>/dev/null; then
+    printStatus "Installing Meld"
+    ${SUDO} apt install meld
+else
+    printStatus "Meld is already installed"
+fi
+
+if ! command transmission-gtk --version &>/dev/null; then
+    printStatus "Installing Transmission"
+    ${SUDO} apt install transmission
+else
+    printStatus "Transmission is already installed"
+fi
--- make-setup.sh
+++ make-setup.sh
@@ -250,66 +250,10 @@ dconf load /org/gnome/terminal/legacy/profiles:/ <gnome-terminal-profiles.dconf
 printStatus "Changing bashrc file"
 cp -fv bashrc ~/.bashrc
 
-if ! command ssh -V &>/dev/null; then
-    printStatus "Installing ssh"
-    sudo apt install openssh-server
-    printStatus "Enabling ssh"
-    sudo systemctl enable ssh
-else
-    printStatus "Openssh-server is already installed"
-fi
 
-printStatus "Installing build-essential kit"
-${SUDO} apt install build-essential
 
-if [[ -f llvm.sh ]]; then
-    printStatus "Removing llvm.sh"
-    rm llvm.sh
-fi
 
-printStatus "Downloading clang installer"
-wget https://apt.llvm.org/llvm.sh
 
-LATEST_CLANG_VERSION=$(awk -F'=' '/^CURRENT_LLVM_STABLE/ {print $2}' llvm.sh)
-printStatus "Latest clang version is ${LATEST_CLANG_VERSION}"
-
-if ! command clang-"${LATEST_CLANG_VERSION}" -v &>/dev/null; then
-    printStatus "Latest clang version is not present"
-    printStatus "Installing llvm-${LATEST_CLANG_VERSION}"
-    ${SUDO} ./llvm.sh
-else
-    printStatus "Latest clang version is already installed"
-fi
-
-if ! command git version &>/dev/null; then
-    printStatus "Installing git"
-    ${SUDO} apt install git-all
-else
-    printStatus "Git is already installed"
-fi
-
-if ! command /snap/bin/cmake --version &>/dev/null; then
-    printStatus "Installing latest CMake using snap"
-    ${SUDO} snap install cmake
-else
-    printStatus "CMake is already installed"
-    printStatus "Trying update"
-    ${SUDO} snap refresh cmake
-fi
-
-if ! command ninja --version &>/dev/null; then
-    printStatus "Installing ninja"
-    ${SUDO} apt install ninja-build
-else
-    printStatus "Ninja is already installed"
-fi
-
-if ! command python3 --version &>/dev/null; then
-    printStatus "Installing python3"
-    ${SUDO} apt install python3
-else
-    printStatus "Python3 is already installed"
-fi
 
 if ! command microsoft-edge --version &>/dev/null; then
     printStatus "Installing Microsoft Edge"
@@ -318,98 +262,6 @@ if ! command microsoft-edge --version &>/dev/null; then
     ${SUDO} apt install microsoft-edge-stable
 else
     printStatus "Microsoft Edge is already installed"
-fi
-
-if [[ ! -f /bin/snap/bitwarden ]]; then
-    printStatus "Installing Bitwarden"
-    ${SUDO} snap install bitwarden
-else
-    printStatus "Bitwarden is already installed"
-    printStatus "Trying update"
-    ${SUDO} snap refresh bitwarden
-fi
-
-if [[ ! -f /bin/snap/clion ]]; then
-    printStatus "Installing CLion"
-    ${SUDO} snap install clion
-else
-    printStatus "CLion is already installed"
-    printStatus "Trying update"
-    ${SUDO} snap refresh clion
-fi
-
-if [[ ! -f /bin/snap/code ]]; then
-    printStatus "Installing VSCode"
-    ${SUDO} snap install code
-    printStatus "Installing C/C++ Extension Pack"
-    code --install-extension ms-vscode.cpptools-extension-pack
-    printStatus "Installing Better C++ syntax"
-    code --install-extension jeff-hykin.better-cpp-syntax
-    printStatus "Installing Code Spell Checker"
-    code --install-extension streetsidesoftware.code-spell-checker
-    printStatus "Installing Doxygen Documentation Generator"
-    code --install-extension cschlosser.doxdocgen
-    printStatus "Installing Git Extension Pack"
-    code --install-extension donjayamanne.git-extension-pack
-    printStatus "Installing Git Blame"
-    code --install-extension waderyan.gitblame
-    printStatus "Installing Git Graph"
-    code --install-extension mhutchie.git-graph
-    printStatus "Installing GitLense"
-    code --install-extension eamodio.gitlens
-    printStatus "Installing QML support"
-    code --install-extension bbenoist.QML
-    printStatus "Installing Shell Format"
-    code --install-extension foxundermoon.shell-format
-    printStatus "Installing ShellCheck"
-    code --install-extension timonwong.shellcheck
-    printStatus "Installing Bash Debug"
-    code --install-extension rogalmic.bash-debug
-else
-    printStatus "VSCode is already installed"
-    printStatus "Trying update"
-    ${SUDO} snap refresh code
-fi
-
-if ! command meld --version &>/dev/null; then
-    printStatus "Installing Meld"
-    ${SUDO} apt install meld
-else
-    printStatus "Meld is already installed"
-fi
-
-if [[ ! -f /bin/snap/zoom-client ]]; then
-    printStatus "Installing Zoom"
-    ${SUDO} snap install zoom-client
-else
-    printStatus "Zoom is already installed"
-    printStatus "Trying update"
-    ${SUDO} snap refresh zoom-client
-fi
-
-if [[ ! -f /bin/snap/gimp ]]; then
-    printStatus "Installing GIMP"
-    ${SUDO} snap install gimp
-else
-    printStatus "GIMP is already installed"
-    printStatus "Trying update"
-    ${SUDO} snap refresh gimp
-fi
-
-if [[ ! -f /bin/snap/remmina ]]; then
-    printStatus "Installing Remmina"
-    ${SUDO} snap install remmina
-else
-    printStatus "Remmina is already installed"
-    printStatus "Trying update"
-    ${SUDO} snap refresh remmina
-fi
-
-if ! command transmission-gtk --version &>/dev/null; then
-    printStatus "Installing Transmission"
-    ${SUDO} apt install transmission
-else
-    printStatus "Transmission is already installed"
 fi
 
 if [[ ${WORKLAPTOP} == 1 ]]; then
