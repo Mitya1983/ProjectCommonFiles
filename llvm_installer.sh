6abcc2d9a6acaf134738eb708d0ab003b1fa51ff
Dmytro Tristan <dmytro.tristan@dxc.com>
2022-08-30 17:43:01 +0200

    Separated into different scripts

---
+++ llvm_installer.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+printStatus() {
+    #if [[ $VERBOSE == true ]]; then
+    printf "[STATUS] %s\n" "${1}"
+    #fi
+}
+
+printError() {
+    printf "[ERROR] %s\n" "${1}"
+}
+
+printWarning() {
+    printf "[WARNING] %s\n" "${1}"
+}
+
+SUDO=""
+
+if [[ $EUID != 0 ]]; then
+    SUDO="sudo"
+fi
+
+if ! command curl -version &>/dev/null; then
+    printStatus "Curl is not installed but needed for this script"
+    printStatus "Installing Curl"
+    ${SUDO} apt install curl
+fi
+
+INTERNET_STATUS=$(curl http://www.msftncsi.com/ncsi.txt)
+if [[ "${INTERNET_STATUS}" != "Microsoft NCSI" ]]; then
+    printError "Internet is not present\nExiting"
+    exit 0
+fi
+
+if [[ -f llvm.sh ]]; then
+    printStatus "Removing llvm.sh"
+    rm llvm.sh
+fi
+
+printStatus "Downloading clang installer"
+wget https://apt.llvm.org/llvm.sh
+
+LATEST_CLANG_VERSION=$(awk -F'=' '/^CURRENT_LLVM_STABLE/ {print $2}' llvm.sh)
+printStatus "Latest clang version is ${LATEST_CLANG_VERSION}"
+
+if ! command clang-"${LATEST_CLANG_VERSION}" -v &>/dev/null; then
+    printStatus "Latest clang version is not present"
+    printStatus "Installing llvm-${LATEST_CLANG_VERSION}"
+    chmod +x "$(pwd)"/llvm.sh
+    ${SUDO} "$(pwd)"/llvm.sh
+else
+    printStatus "Latest clang version is already installed"
+fi
---
+++ snap_installs.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+
+printStatus() {
+    #if [[ $VERBOSE == true ]]; then
+    printf "[STATUS] %s\n" "${1}"
+    #fi
+}
+
+printError() {
+    printf "[ERROR] %s\n" "${1}"
+}
+
+printWarning() {
+    printf "[WARNING] %s\n" "${1}"
+}
+
+SUDO=""
+
+if [[ $EUID != 0 ]]; then
+    SUDO="sudo"
+fi
+
+if ! command curl -version &>/dev/null; then
+    printStatus "Curl is not installed but needed for this script"
+    printStatus "Installing Curl"
+    ${SUDO} apt install curl
+fi
+
+INTERNET_STATUS=$(curl http://www.msftncsi.com/ncsi.txt)
+if [[ "${INTERNET_STATUS}" != "Microsoft NCSI" ]]; then
+    printError "Internet is not present\nExiting"
+    exit 0
+fi
+
+if [[ ! -f /bin/snap/cmake ]]; then
+    printStatus "Installing latest CMake using snap"
+    ${SUDO} snap install cmake --classic
+else
+    printStatus "CMake is already installed"
+    printStatus "Trying update"
+    ${SUDO} snap refresh cmake
+fi
+
+if [[ ! -f /bin/snap/bitwarden ]]; then
+    printStatus "Installing Bitwarden"
+    ${SUDO} snap install bitwarden
+else
+    printStatus "Bitwarden is already installed"
+    printStatus "Trying update"
+    ${SUDO} snap refresh bitwarden
+fi
+
+if [[ ! -f /bin/snap/clion ]]; then
+    printStatus "Installing CLion"
+    ${SUDO} snap install clion
+else
+    printStatus "CLion is already installed"
+    printStatus "Trying update"
+    ${SUDO} snap refresh clion
+fi
+
+if [[ ! -f /bin/snap/code ]]; then
+    printStatus "Installing VSCode"
+    ${SUDO} snap install code
+    printStatus "Installing C/C++ Extension Pack"
+    code --install-extension ms-vscode.cpptools-extension-pack
+    printStatus "Installing Better C++ syntax"
+    code --install-extension jeff-hykin.better-cpp-syntax
+    printStatus "Installing Code Spell Checker"
+    code --install-extension streetsidesoftware.code-spell-checker
+    printStatus "Installing Doxygen Documentation Generator"
+    code --install-extension cschlosser.doxdocgen
+    printStatus "Installing Git Extension Pack"
+    code --install-extension donjayamanne.git-extension-pack
+    printStatus "Installing Git Blame"
+    code --install-extension waderyan.gitblame
+    printStatus "Installing Git Graph"
+    code --install-extension mhutchie.git-graph
+    printStatus "Installing GitLense"
+    code --install-extension eamodio.gitlens
+    printStatus "Installing QML support"
+    code --install-extension bbenoist.QML
+    printStatus "Installing Shell Format"
+    code --install-extension foxundermoon.shell-format
+    printStatus "Installing ShellCheck"
+    code --install-extension timonwong.shellcheck
+    printStatus "Installing Bash Debug"
+    code --install-extension rogalmic.bash-debug
+else
+    printStatus "VSCode is already installed"
+    printStatus "Trying update"
+    ${SUDO} snap refresh code
+fi
+
+if [[ ! -f /bin/snap/zoom-client ]]; then
+    printStatus "Installing Zoom"
+    ${SUDO} snap install zoom-client
+else
+    printStatus "Zoom is already installed"
+    printStatus "Trying update"
+    ${SUDO} snap refresh zoom-client
+fi
+
+if [[ ! -f /bin/snap/gimp ]]; then
+    printStatus "Installing GIMP"
+    ${SUDO} snap install gimp
+else
+    printStatus "GIMP is already installed"
+    printStatus "Trying update"
+    ${SUDO} snap refresh gimp
+fi
+
+if [[ ! -f /bin/snap/remmina ]]; then
+    printStatus "Installing Remmina"
+    ${SUDO} snap install remmina
+else
+    printStatus "Remmina is already installed"
+    printStatus "Trying update"
+    ${SUDO} snap refresh remmina
+fi
